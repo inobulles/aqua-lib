39a8eac97377d97dd721178d91783fa78645f973
Obiwac <obiwac@gmail.com>
2021-08-15 10:08:57 +0200

    Added device handling functions to `root.h`

--- c/root.h
+++ c/root.h
@@ -1,8 +1,42 @@
 #if !defined(__AQUA_LIB__ROOT)
 #define __AQUA_LIB__ROOT
 
-void testing_function(void) {
-	printf("%s\n", __func__);
+#include <stdio.h>
+#include <stdint.h>
+
+typedef uint64_t (*kos_query_device_t) (uint64_t _, uint64_t name);
+typedef uint64_t (*kos_send_device_t) (uint64_t _, uint64_t device, uint64_t command, uint64_t data);
+
+static kos_query_device_t kos_query_device = NULL;
+static kos_send_device_t kos_send_device = NULL;
+
+void aqua_set_kos_functions(kos_query_device_t _kos_query_device, kos_send_device_t _kos_send_device) {
+	kos_query_device = _kos_query_device;
+	kos_send_device = _kos_send_device;
+}
+
+static void must_run_with_aqua_kos(void) {
+	fprintf(stderr, "You must run this program with the AQUA KOS\n");
+}
+
+typedef uint64_t device_t;
+
+device_t query_device(const char* name) {
+	if (!kos_query_device) {
+		must_run_with_aqua_kos();
+		return -1;
+	}
+	
+	return kos_query_device(0, (uint64_t) name);
+}
+
+uint64_t send_device(device_t device, uint16_t command, void* data) {
+	if (!kos_send_device) {
+		must_run_with_aqua_kos();
+		return -1;
+	}
+
+	return kos_send_device(0, device, (uint64_t) command, (uint64_t) data);
 }
 
 #endif
