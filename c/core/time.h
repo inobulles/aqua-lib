79ef01f4905cc722565ef38e658b9c7a268c7e62
Obiwac <obiwac@gmail.com>
2021-08-15 10:14:48 +0200

    Added `core/time.h`

---
+++ c/core/time.h
@@ -0,0 +1,16 @@
+#if !defined(__AQUA_LIB__CORE_TIME)
+#define __AQUA_LIB__CORE_TIME
+
+#include <root.h>
+
+device_t time_device = -1;
+
+uint64_t get_unix_time(void) {
+	if (time_device == -1) {
+		time_device = query_device("core.time");
+	}
+
+	return send_device(time_device, 0x7578, NULL);
+}
+
+#endif
