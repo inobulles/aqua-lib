083bdd21c9cd6fbcfc997c3a45840bec961f92d5
obiwac <obiwac@gmail.com>
2021-08-28 00:12:45 +0200

    Added WIP `aquabsd/alps/vga.h` library

---
+++ c/aquabsd/alps/font.h
@@ -0,0 +1,27 @@
+#if !defined(__AQUA_LIB__AQUABSD_ALPS_FONT)
+#define __AQUA_LIB__AQUABSD_ALPS_FONT
+
+#include <root.h>
+
+// TODO error handling
+
+static device_t font_device = -1;
+typedef uint64_t font_t;
+
+font_t font_load(const char* path) {
+	if (font_device == -1) {
+		font_device = query_device("aquabsd.alps.font");
+	}
+
+	return send_device(font_device, 0x6C66, (uint64_t[]) { (uint64_t) path });
+}
+
+int font_draw(font_t font, const char* string, float red, float green, float blue, float alpha, int64_t wrap_width, int64_t wrap_height, uint8_t** bitmap_reference, uint64_t* width_reference, uint64_t* height_reference) {
+	return (int) send_device(font_device, 0x6466, (uint64_t[]) { font, (uint64_t) string, *(uint64_t*) &red, *(uint64_t*) &green, *(uint64_t*) &blue, *(uint64_t*) &alpha, (uint64_t) wrap_width, (uint64_t) wrap_height, (uint64_t) bitmap_reference, (uint64_t) width_reference, (uint64_t) height_reference });
+}
+
+void font_free(font_t font) {
+	send_device(font_device, 0x6666, (uint64_t[]) { font });
+}
+
+#endif
---
+++ c/aquabsd/alps/vga.h
@@ -0,0 +1,45 @@
+#if !defined(__AQUA_LIB__AQUABSD_ALPS_VGA)
+#define __AQUA_LIB__AQUABSD_ALPS_VGA
+
+#include <root.h>
+
+// TODO error handling
+
+static device_t vga_device = -1;
+
+typedef struct {
+	uint64_t text;
+
+	uint64_t width, height;
+	uint64_t bpp, fps;
+} vga_mode_t;
+
+int vga_get_mode_count(void) {
+	return (int) send_device(vga_device, 0x6D63, NULL);
+}
+
+vga_mode_t* vga_get_modes(void) {
+	return (vga_mode_t*) send_device(vga_device, 0x6D64, NULL);
+}
+
+int vga_set_mode(vga_mode_t* mode) {
+	return (int) send_device(vga_device, 0x736D, (uint64_t[]) { (uint64_t) mode });
+}
+
+void* vga_get_framebuffer(void) {
+	return (void*) send_device(vga_device, 0x6662, NULL);
+}
+
+int vga_flip(void) {
+	return (int) send_device(vga_device, 0x666C, NULL);
+}
+
+int vga_init(void) {
+	if (vga_device == -1) {
+		vga_device = query_device("aquabsd.alps.vga");
+	}
+
+	return vga_get_mode_count() == -1 ? -1 : 0;
+}
+
+#endif
