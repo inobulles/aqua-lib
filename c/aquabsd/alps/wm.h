b362ef82bfed6c91ac8f6982e9346ea23f708a6f
obiwac <obiwac@gmail.com>
2021-12-01 07:44:50 +0100

    wm: replaced `wm_get_overlay` with `wm_make_compositing`

--- c/aquabsd/alps/wm.h
+++ c/aquabsd/alps/wm.h
@@ -65,13 +65,8 @@ int wm_register_cb(wm_t wm, wm_cb_t type, int (*cb) (uint64_t wm, uint64_t win, 
 	return send_device(wm_device, 0x7263, (uint64_t[]) {wm, type, (uint64_t) cb, (uint64_t) param});
 }
 
-win_t* wm_get_overlay(wm_t wm) {
-	uint64_t _win = send_device(wm_device, 0x6F77, (uint64_t[]) { wm });
-	
-	win_t* win = wm_get_root(wm); // base ourselves off the root window
-	win->win = _win;
-
-	return win;
+void wm_make_compositing(wm_t wm) {
+	send_device(wm_device, 0x6D63, (uint64_t[]) { wm });
 }
 
 #endif
