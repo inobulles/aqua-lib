2d2f2d8fa46196fe4a421a5de1abf6aa25c2a112
obiwac <obiwac@gmail.com>
2021-12-22 21:34:36 +0100

    win: synchronizing window also synchronizes WM X/Y resolution

--- c/aquabsd/alps/win.h
+++ c/aquabsd/alps/win.h
@@ -11,6 +11,7 @@ typedef enum {
 typedef struct {
 	int x_pos, y_pos;
 	unsigned x_res, y_res;
+	unsigned wm_x_res, wm_y_res;
 
 	uint64_t win;
 } win_t;
@@ -66,11 +67,14 @@ int win_loop(win_t* win) {
 }
 
 void win_sync(win_t* win) {
-	win->x_pos = send_device(win_device, 0x7870, (uint64_t[]) { win->win });
-	win->y_pos = send_device(win_device, 0x7970, (uint64_t[]) { win->win });
+	win->x_pos    = send_device(win_device, 0x7870, (uint64_t[]) { win->win });
+	win->y_pos    = send_device(win_device, 0x7970, (uint64_t[]) { win->win });
 
-	win->x_res = send_device(win_device, 0x7872, (uint64_t[]) { win->win });
-	win->y_res = send_device(win_device, 0x7972, (uint64_t[]) { win->win });
+	win->x_res    = send_device(win_device, 0x7872, (uint64_t[]) { win->win });
+	win->y_res    = send_device(win_device, 0x7972, (uint64_t[]) { win->win });
+
+	win->wm_x_res = send_device(win_device, 0x7778, (uint64_t[]) { win->win });
+	win->wm_y_res = send_device(win_device, 0x7779, (uint64_t[]) { win->win });
 }
 
 void win_grab_focus(win_t* win) {
