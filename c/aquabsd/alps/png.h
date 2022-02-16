552ad9642ce27c83aed236750eb0941081d20ed1
obiwac <obiwac@gmail.com>
2022-02-16 20:59:24 +0100

    png: `png_load_ptr`

--- c/aquabsd/alps/png.h
+++ c/aquabsd/alps/png.h
@@ -16,6 +16,14 @@ png_t png_load(const char* path) {
 	return send_device(png_device, 0x6C64, (uint64_t[]) { (uint64_t) path });
 }
 
+png_t png_load_ptr(void* ptr) {
+	if (png_device == -1) {
+		png_device = query_device("aquabsd.alps.png");
+	}
+
+	return send_device(png_device, 0x6C70, (uint64_t[]) { (uint64_t) ptr });
+}
+
 int png_draw(png_t png, uint8_t** bitmap_reference, uint64_t* bpp_reference, uint64_t* width_reference, uint64_t* height_reference) {
 	return (int) send_device(png_device, 0x6477, (uint64_t[]) { png, (uint64_t) bitmap_reference, (uint64_t) bpp_reference, (uint64_t) width_reference, (uint64_t) height_reference });
 }
