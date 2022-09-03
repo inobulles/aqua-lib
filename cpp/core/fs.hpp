cd2c5d898f472cb268ddb03d277f77ceea6e3e11
obiwac <obiwac@gmail.com>
2022-09-04 00:25:03 +0200

    cpp/fs: Try my hand at exceptions

--- cpp/core/fs.hpp
+++ cpp/core/fs.hpp
@@ -7,6 +7,19 @@ namespace aqua_libc {
 }
 
 namespace aqua::core::fs {
+	class Generic_error : public std::exception {
+		std::string msg;
+
+	public:
+
+		Generic_error(std::string msg) : msg(msg) {
+		}
+
+		std::string what(void) {
+			return "Generic AQUA filesystem error: " + this->msg;
+		}
+	};
+
 	class Descr {
 		aqua_libc::fs_descr_t descr;
 
@@ -14,6 +27,10 @@ namespace aqua::core::fs {
 
 		Descr(const std::string drive, const std::string path, aqua_libc::fs_flags_t flags) {
 			descr = aqua_libc::fs_open(drive.c_str(), path.c_str(), flags);
+
+			if (!descr) {
+				throw Generic_error("Failed to open '" + drive + ":" + path + "'");
+			}
 		}
 
 		~Descr() {
@@ -28,18 +45,26 @@ namespace aqua::core::fs {
 			return aqua_libc::fs_mmap(descr);
 		}
 
-		aqua_libc::fs_err_t read(void* buf, size_t len) {
-			return aqua_libc::fs_read(descr, buf, len);
+		void read(void* buf, size_t len) {
+			aqua_libc::fs_err_t err = aqua_libc::fs_read(descr, buf, len);
+
+			if (err != aqua_libc::FS_ERR_SUCCESS) {
+				throw Generic_error("Failed to read from file");
+			}
 		}
 
-		aqua_libc::fs_err_t write(const void* buf, size_t len) {
-			return aqua_libc::fs_write(descr, buf, len);
+		void write(const void* buf, size_t len) {
+			aqua_libc::fs_err_t err = aqua_libc::fs_write(descr, buf, len);
+
+			if (err != aqua_libc::FS_ERR_SUCCESS) {
+				throw Generic_error("Failed to write to file");
+			}
 		}
 
 		// helper functions
 
-		aqua_libc::fs_err_t write(std::string str) {
-			return write(str.c_str(), str.size());
+		void write(std::string str) {
+			write(str.c_str(), str.size());
 		}
 	};
 }
